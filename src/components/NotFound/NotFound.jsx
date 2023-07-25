9b6176eded119db8d8bce717cf16aca3ad178f80
Tresor Sawasawa <tresorsawasawa@gmail.com>
2023-07-26 01:04:36 +0200

    Add the Notfound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <div>Not Found Page</div>;
+const NotFound = () => (
+  <section className="container">
+    <Link to="/">Go Back To Home Page</Link>
+  </section>
+);
 
 export default NotFound;
