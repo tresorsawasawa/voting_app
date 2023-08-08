bdbd99c263fca7028ea06c035a0e80f567ff30a6
Tresor Sawasawa <tresorsawasawa@gmail.com>
2023-08-08 16:46:32 +0200

    Update default styling and add new components

--- src/components/Login/Login.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Login = () => <h2>Login Page</h2>;
-
-export default Login;
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Menu = () => <h2>Menu</h2>;
+
+export default Menu;
