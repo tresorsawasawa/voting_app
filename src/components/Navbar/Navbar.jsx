38106dfc6cab7d39dc92a764ca0ae75b3eecc6bf
Tresor Sawasawa <tresorsawasawa@gmail.com>
2023-08-08 21:54:35 +0200

    Add Navbar and default styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,36 +2,50 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import uuid from 'react-uuid';
 
+import './Navbar.scss';
+import UserImage from '../../assets/images/user_avatar.png';
+
 const Navbar = () => {
   const links = [
     {
       id: uuid(),
       name: 'Login',
       to: '/sign_in',
-      className: 'clickable',
+      className: 'clickable icon',
     },
     {
       id: uuid(),
       name: 'Sign Up',
       to: '/sign_up',
-      className: 'clickable',
+      className: 'clickable icon',
     },
   ];
 
+  console.log(links);
+
   return (
-    links.map(
-      ({
-        id, name, to, className,
-      }) => (
-        <NavLink
-          key={id}
-          to={to}
-          className={className}
-        >
-          {name}
-        </NavLink>
-      ),
-    )
+    <header className="header">
+      <div className="logoContainer">
+        <h2 className="logo">
+          <NavLink to="/">
+            Vote
+            {' '}
+            <span className="dot">.</span>
+          </NavLink>
+        </h2>
+      </div>
+
+      <div className="icons">
+        <img src={UserImage} alt="user avatar" className="userImage" />
+        {/* {links.map(({
+          id, name, to, className,
+        }) => (
+          <NavLink key={id} to={to} className={className}>
+            {name}
+          </NavLink>
+        ))} */}
+      </div>
+    </header>
   );
 };
 
